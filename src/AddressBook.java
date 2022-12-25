ae866df5068618c693f8cc44a30916995e8f97a7
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-25 16:24:14 +0530

    create a contact and edit using name

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -20,4 +20,53 @@ public class AddressBook {
         System.out.println("Enter the Mobile no : ");
         contactPerson.setPhoneNo(sc.next());
     }
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "contactList=" + contactList +
+                '}';
+    }
+
+    public void editContact() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first name:");
+        String name = sc.next();
+        for (Contact contactPerson : contactList) {
+            if (name.equals(contactPerson.getName())) {
+                System.out.println("What you want to edit for the contact");
+                System.out.println("1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Zip Code\n7.Mobile Number\n8.Email id");
+                int choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter the First Name :");
+                        contactPerson.setName(sc.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter the Last Name :");
+                        contactPerson.setLastName(sc.next());
+                        break;
+                    case 3:
+                        System.out.println("Enter the Address :");
+                        contactPerson.setAddress(sc.next());
+                        break;
+                    case 4:
+                        System.out.println("Enter the City :");
+                        contactPerson.setCity(sc.next());
+                        break;
+                    case 5:
+                        System.out.println("Enter the State :");
+                        contactPerson.setState(sc.next());
+                        break;
+                    case 6:
+                        System.out.println("Enter the Zip code :");
+                        contactPerson.setZipCode(sc.next());
+                        break;
+                    case 7:
+                        System.out.println("Enter the Mobile Number :");
+                        contactPerson.setPhoneNo(sc.next());
+                        break;
+                }
+            }
+        }
+    }
 }
--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome To Address-book");
         AddressBook familyBook = new AddressBook();
-        System.out.println("Ability to create new contact");
-        familyBook.addContact();
+        System.out.println("WelCome To AddressBook Program");
+        Scanner scanner = new Scanner(System.in);
+        int opration;
+        do {
+            System.out.println("1. ADD CONTACT \n2. DISPLAY CONTACT \n3 EDIT \n4 Delete \n5. EXIT ");
+            System.out.println("Enter the Operation Number");
+            opration = scanner.nextInt();
+            switch (opration) {
+                case 1:
+                    familyBook.addContact();
+                    break;
+                case 2:
+                    System.out.println(familyBook);
+                    break;
+                case 3:
+                    familyBook.editContact();
+                    break;
+                case 4:
+                    System.out.println("Exiting");
+                    break;
+                default:
+                    System.out.println("Enter The Wrong Opration Number");
+            }
+        } while (opration != 5);
     }
 }
