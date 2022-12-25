61098b262211f2db74c30e7042b6e8e7227e9d9e
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-25 16:18:08 +0530

    create a nwe contact

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -2,7 +2,7 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Address-book");
         AddressBook familyBook = new AddressBook();
-        System.out.println("WelCome To AddressBook Program");
+        System.out.println("Ability to create new contact");
         familyBook.addContact();
     }
 }
